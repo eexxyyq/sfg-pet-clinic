9e718d52ca566b7fc6bb72b088bfe42ed06cf3aa
eexxyyq <eexxyyq@gmail.com>
2020-05-03 17:21:15 +0300

    enable auto generated ids

--- sfg-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -19,14 +19,12 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("FIRST_FIRST_NAME");
         owner1.setLastName("FIRST_LAST_NAME");
-        owner1.setId(1L);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("SECOND_FIRST_NAME");
         owner2.setLastName("SECOND_LAST_NAME");
-        owner2.setId(2L);
 
         ownerService.save(owner2);
 
@@ -36,14 +34,12 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("FIRST_FIRST_NAME");
         vet1.setLastName("FIRST_LAST_NAME");
-        vet1.setId(1L);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("SECOND_FIRST_NAME");
         vet2.setLastName("SECOND_LAST_NAME");
-        vet2.setId(2L);
 
         vetService.save(vet2);
 
