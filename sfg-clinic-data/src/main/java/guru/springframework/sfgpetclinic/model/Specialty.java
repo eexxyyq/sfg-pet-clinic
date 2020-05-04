90500c7f8f4ca52a0e49ecf60be831b86aaf8c75
eexxyyq <eexxyyq@gmail.com>
2020-05-04 16:09:23 +0300

    added Specialty entity

--- sfg-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ sfg-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -3,10 +3,8 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Getter
 @Setter
-public class Vet extends Person {
-    private Set<Specialty> specialties;
+public class Specialty extends BaseEntity {
+    private String description;
 }
