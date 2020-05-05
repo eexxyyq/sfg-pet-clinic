9d0b3bf4b05ceab97845026adf046c11099619f5
eexxyyq <eexxyyq@gmail.com>
2020-05-05 16:35:52 +0300

    update jpa entities in project (pet and visit)

--- sfg-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -23,4 +25,7 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 }
