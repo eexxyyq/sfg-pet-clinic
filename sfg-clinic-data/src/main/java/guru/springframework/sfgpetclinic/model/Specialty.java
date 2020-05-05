340aa942fd5e96009b6df1459aeeadc99377e9be
eexxyyq <eexxyyq@gmail.com>
2020-05-05 16:30:36 +0300

    update jpa entities in project (vets and spec)

--- sfg-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ sfg-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -3,8 +3,15 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 }
